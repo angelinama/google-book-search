868b15a46a565e05c631b93293c6d7f21af72f91
Bingqing(Angelina) Ma <angelina890308@gmail.com>
2021-03-23 19:35:46 -0700

    create client side skeleton codes

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,23 @@
-import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>A google book search</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
-
 
 export default App;
